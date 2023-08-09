791b01e52fe511fcfb71bca3c1c2df91658615b5
chikushihayato <chikushi.jambo@gmail.com>
2023-08-09 13:01:40 +0900

    pokemon list display

--- app/src/main/java/com/example/pokemonapp/MyAdapter.java
+++ app/src/main/java/com/example/pokemonapp/MyAdapter.java
@@ -7,16 +7,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
     Context context;
-    List<Item> items;
+    List<Pokemon> Pokemons;
 
-    public MyAdapter(Context context, List<Item> items) {
+    public MyAdapter(Context context, List<Pokemon> Pokemons) {
         this.context = context;
-        this.items = items;
+        this.Pokemons = Pokemons;
     }
 
     @NonNull
@@ -27,11 +29,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.imageView.setImageResource(items.get(position).getImage());
+        int id = Pokemons.get(position).getId();
+        String numText = String.valueOf(id);
+        holder.idView.setText("No. " + numText);
+        String imageUrl = Pokemons.get(position).getImageUrl();
+        Glide.with(holder.itemView).load(imageUrl).into(holder.pokemonView);
+        holder.nameView.setText(Pokemons.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return Pokemons.size();
     }
 }
